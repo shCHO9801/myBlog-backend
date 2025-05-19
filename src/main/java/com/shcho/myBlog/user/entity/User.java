3f315e93dc62087b8b9392f07c364086c187997e
sunghyun <csh980116@naver.com>
2025-05-19 13:20:08 +0900

    feat: User 엔티티 및 Repository 정의

--- src/main/java/com/shcho/myBlog/user/entity/User.java
+++ src/main/java/com/shcho/myBlog/user/entity/User.java
@@ -1,24 +1,27 @@
 package com.shcho.myBlog.user.entity;
 
+import com.shcho.myBlog.libs.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+@EntityListeners(AuditingEntityListener.class)
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, nullable = false)
-    private String userId;
+    private String username;
 
     @Column(nullable = false)
     private String password;
@@ -27,6 +30,6 @@ public class User {
     private String nickname;
 
     @Enumerated(EnumType.STRING)
-    @Column(columnDefinition = "VARCHAR(50")
+    @Column(columnDefinition = "VARCHAR(50)")
     private Role role;
 }
