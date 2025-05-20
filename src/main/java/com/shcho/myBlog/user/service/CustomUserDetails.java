a4822b41448a40e410d0fe05dadadb269cfc5cd1
sunghyun <csh980116@naver.com>
2025-05-20 13:37:39 +0900

    refactor : 사용하지 않는 import 문 제거

--- src/main/java/com/shcho/myBlog/user/service/CustomUserDetails.java
+++ src/main/java/com/shcho/myBlog/user/service/CustomUserDetails.java
@@ -3,7 +3,6 @@ package com.shcho.myBlog.user.service;
 import com.shcho.myBlog.user.entity.Role;
 import com.shcho.myBlog.user.entity.User;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -30,10 +29,33 @@ public class CustomUserDetails implements UserDetails {
         return Collections.singleton((GrantedAuthority) () -> "ROLE_" + role.name());
     }
 
-    @Override public String getPassword() { return password; }
-    @Override public String getUsername() { return username; }
-    @Override public boolean isAccountNonExpired() { return true; }
-    @Override public boolean isAccountNonLocked() { return true; }
-    @Override public boolean isCredentialsNonExpired() { return true; }
-    @Override public boolean isEnabled() { return true; }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
