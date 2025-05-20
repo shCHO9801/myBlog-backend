465fdc2fb1b410fce507b091af7882cda88ae7b6
sunghyun <csh980116@naver.com>
2025-05-20 13:36:25 +0900

    feat: CustomUserDetails에 userId 추가 및 getter 정의

---
+++ src/main/java/com/shcho/myBlog/user/service/CustomUserDetails.java
@@ -0,0 +1,39 @@
+package com.shcho.myBlog.user.service;
+
+import com.shcho.myBlog.user.entity.Role;
+import com.shcho.myBlog.user.entity.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final Long userId;
+    private final String username;
+    private final String password;
+    private final Role role;
+
+    public CustomUserDetails(User user) {
+        this.userId = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.role = user.getRole();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton((GrantedAuthority) () -> "ROLE_" + role.name());
+    }
+
+    @Override public String getPassword() { return password; }
+    @Override public String getUsername() { return username; }
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
+}
