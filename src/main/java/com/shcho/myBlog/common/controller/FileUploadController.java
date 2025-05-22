52181b9bb985dd630374634147348f27cf63be18
sunghyun <csh980116@naver.com>
2025-05-22 14:25:02 +0900

    feat : 파일 업로드 API 구현

---
+++ src/main/java/com/shcho/myBlog/common/controller/FileUploadController.java
@@ -0,0 +1,28 @@
+package com.shcho.myBlog.common.controller;
+
+import com.shcho.myBlog.common.dto.FileUploadResponseDto;
+import com.shcho.myBlog.common.service.S3Service;
+import com.shcho.myBlog.user.service.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/files")
+@RequiredArgsConstructor
+public class FileUploadController {
+
+    private final S3Service s3Service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<FileUploadResponseDto> uploadFile(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestPart MultipartFile file,
+            @RequestParam("type") String type
+    ) {
+        String username = userDetails.getUsername();
+        return ResponseEntity.ok(s3Service.uploadFile(file, type, username));
+    }
+}
