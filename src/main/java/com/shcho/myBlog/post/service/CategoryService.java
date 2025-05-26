41bf1b751797742f14558c0b4448bc45afeff06f
sunghyun <csh980116@naver.com>
2025-05-26 14:33:46 +0900

    feat : Category 생성, 조회, 삭제 API 구성 (POST, GET, DELETE)

---
+++ src/main/java/com/shcho/myBlog/post/service/CategoryService.java
@@ -0,0 +1,55 @@
+package com.shcho.myBlog.post.service;
+
+import com.shcho.myBlog.libs.exception.CustomException;
+import com.shcho.myBlog.libs.exception.ErrorCode;
+import com.shcho.myBlog.post.dto.CategoryResponseDto;
+import com.shcho.myBlog.post.entity.Category;
+import com.shcho.myBlog.post.repository.CategoryRepository;
+import com.shcho.myBlog.user.entity.User;
+import com.shcho.myBlog.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
+
+    public CategoryResponseDto createCategory(Long userId, String name) {
+        User user = userRepository.getReferenceById(userId);
+
+        if(categoryRepository.existsByUserIdAndName(user.getId(),name)) {
+            throw new CustomException(ErrorCode.DUPLICATE_CATEGORY);
+        }
+
+        Category category = Category.of(name, user);
+        categoryRepository.save(category);
+
+        return CategoryResponseDto.from(category);
+    }
+
+    public List<CategoryResponseDto> getCategories(Long userId) {
+        return categoryRepository.findAllByUserId(userId)
+                .stream()
+                .map(CategoryResponseDto::from)
+                .toList();
+    }
+
+    @Transactional
+    public String deleteCategory(Long userId, Long categoryId) {
+        User user = userRepository.getReferenceById(userId);
+        Category category = categoryRepository.getReferenceById(categoryId);
+
+        if(!user.equals(category.getUser())) {
+            throw new CustomException(ErrorCode.UNAUTHORIZED_CATEGORY_ACCESS);
+        }
+
+        categoryRepository.deleteById(categoryId);
+        return "카테고리가 삭제되었습니다.";
+    }
+}
